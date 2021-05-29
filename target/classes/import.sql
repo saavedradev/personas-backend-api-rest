480ac31ce111ba217c668667fc5f7aac116802e8
saavedradev <saavedra.dev@gmail.com>
2021-05-29 17:05:47 -0500

    get data people service

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,3 +1,3 @@
 
 
-INSERT INTO people (identification, fullName, birth, gender, fatherId, motherId, create_at) VALUES(1, '123', 'Andres soto', '2018-01-01', 'M','', '', '2018-01-01');
+INSERT INTO people (identification, full_name, birth, gender, father_id, mother_id, create_at) VALUES('123', 'Andres soto', '2018-01-01', 'M','', '', '2018-01-01');
