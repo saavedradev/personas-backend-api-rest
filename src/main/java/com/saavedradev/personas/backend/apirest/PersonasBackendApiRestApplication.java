802f2e8ff80938cd9f91964b8b95f9059316335c
saavedradev <saavedra.dev@gmail.com>
2021-05-29 16:14:37 -0500

    configuration project

---
+++ src/main/java/com/saavedradev/personas/backend/apirest/PersonasBackendApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.saavedradev.personas.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasBackendApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasBackendApiRestApplication.class, args);
+	}
+
+}
