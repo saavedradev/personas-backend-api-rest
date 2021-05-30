49eff4043892b9f91ec522816129ceed6ed66127
saavedradev <saavedra.dev@gmail.com>
2021-05-29 20:48:36 -0500

    changes to update person

--- src/main/java/com/saavedradev/personas/backend/apirest/controllers/PersonRestController.java
+++ src/main/java/com/saavedradev/personas/backend/apirest/controllers/PersonRestController.java
@@ -1,8 +1,5 @@
 package com.saavedradev.personas.backend.apirest.controllers;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -104,5 +102,46 @@ public class PersonRestController {
 		response.put("mensaje", "La persona ha sido eliminada con éxito!");
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
+	
+	@PutMapping("/people/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> update(@Valid @RequestBody Person person, BindingResult result, @PathVariable Long id) {
+		Person personActual = personService.findById(id);
+		Person personUpdate = null;
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '"+ err.getField() +"' " +err.getDefaultMessage())
+					.collect(Collectors.toList());
+			response.put("errors", errors);
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);	
+		}
+		
+		if( personActual == null) {
+			response.put("mensaje", "Error: no se puede editar, la persona con ID: ".concat(id.toString().concat(" no existe en la bd!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);	
+		}
+		try {
+			personActual.setIdentification(person.getIdentification());	
+			personActual.setFullName(person.getFullName());
+			personActual.setGender(person.getGender());
+			personActual.setMotherId(person.getMotherId());
+			personUpdate = personService.save(personActual);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el persona en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);	
+		}
+
+		response.put("mensaje", "La persona ha sido actualizado con éxito!");
+		response.put("cliente", personUpdate);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);	
+	}
+	
 
 }
