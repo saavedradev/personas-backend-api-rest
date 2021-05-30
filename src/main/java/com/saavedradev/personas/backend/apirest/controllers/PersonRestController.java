95c10984ba5068f21ade69060277ff43adf4571c
saavedradev <saavedra.dev@gmail.com>
2021-05-29 19:43:11 -0500

    changes to crd

--- src/main/java/com/saavedradev/personas/backend/apirest/controllers/PersonRestController.java
+++ src/main/java/com/saavedradev/personas/backend/apirest/controllers/PersonRestController.java
@@ -1,28 +1,108 @@
 package com.saavedradev.personas.backend.apirest.controllers;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saavedradev.personas.backend.apirest.models.entity.Person;
 import com.saavedradev.personas.backend.apirest.models.services.IPersonService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class PersonRestController {
-	
+
 	@Autowired
 	private IPersonService personService;
-	
+
 	@GetMapping("/people")
-	public List<Person> index(){
+	public List<Person> index() {
 		return personService.findAll();
-		
+
+	}
+
+	@GetMapping("/people/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Person person = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			person = personService.findById(id);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if (person == null) {
+			response.put("mensaje", "La persona con ID: ".concat(id.toString().concat(" no existe en la bd!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Person>(person, HttpStatus.OK);
+	}
+
+	@PostMapping("/people")
+	public ResponseEntity<?> create(@Valid @RequestBody Person person, BindingResult result) {
+		Person personNew = null;
+		Map<String, Object> response = new HashMap<>();
+
+		if (result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			response.put("errors", errors);
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			personNew = personService.save(person);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "La persona ha sido creada con éxito");
+		response.put("persona", personNew);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/people/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			personService.delete(id);
+		}catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar la persona de la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);	
+		}
+		response.put("mensaje", "La persona ha sido eliminada con éxito!");
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/saavedradev/personas/backend/apirest/models/entity/Person.java
+++ src/main/java/com/saavedradev/personas/backend/apirest/models/entity/Person.java
@@ -8,9 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 
 @Entity
@@ -20,15 +22,22 @@ public class Person implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@NotEmpty(message="no puede estar vacio")
 	@Column(nullable = false, unique = true)
 	private String identification;
+	
+	@NotEmpty(message="no puede estar vacio")
 	@Column(nullable = false)
 	private String fullName;
+	
 	@Column(nullable = false)
 	private Date birth;
+	
+	@NotEmpty(message="no puede estar vacio")
 	@Column(nullable = false)
 	private String gender; // M for male, F for female
+	
 	@Column(nullable = false)
 	private String fatherId;
 	@Column(nullable = false)
@@ -99,6 +108,11 @@ public class Person implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
 	}
 
 	/**
