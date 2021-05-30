dc60a0174127647dc538f83f59c00efafe521199
saavedradev <saavedra.dev@gmail.com>
2021-05-29 19:04:05 -0500

    maven configuration

---
+++ bin/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO people (identification, full_name, birth, gender, father_id, mother_id, create_at) VALUES('123', 'Andres soto', '2018-01-01', 'M','', '', '2018-01-01');
+INSERT INTO people (identification, full_name, birth, gender, father_id, mother_id, create_at) VALUES('124', 'luisa', '2018-01-01', 'F','123', '', '2018-01-01');
